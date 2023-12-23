a395a6c0f7a41dcefce20fd854f38731474015e2
Jayanta <bjayanta.me@gmail.com>
2023-12-23 17:28:20 +0600

    create a serve

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello node api.');
 });
 
+app.get('/blog', (req, res) => {
+    res.send('Hello blog. I am Jayanta.');
+});
+
 app.listen(3000, () => {
     console.log(`Node API app is running on port 3000`);
 });
